fafe4dbc60bd3ba60d374f5e36a681bdae5bbc82
Gordon B. Isnor <gordonbisnor@gmail.com>
2015-03-20 11:03:13 -0300

    0.0.1 option to paginate via data attributes, to work with Ember

--- someFrillsCarousel.js
+++ someFrillsCarousel.js
@@ -1,17 +1,17 @@
 /*!
-	someFrillsCarousel v1.0 - 2014-01-26
-	Simple carousel with pagination, delay and speed settings
-	(c) 2014 Gordon B. Isnor - http://github.com/someFrillsCarousel
+  someFrillsCarousel 0.0.1 - 2014-01-26
+  Simple carousel with pagination, delay and speed settings
+  (c) 2014 Gordon B. Isnor - http://github.com/someFrillsCarousel
 */
-(function($) {
+(function ($) {
 
-  $.fn.someFrillsCarousel = function(args) {
+  $.fn.someFrillsCarousel = function (args) {
 
     return $(this).each(function () {
 
-    	/* config */
-  		var carouselDelay = args && args.delay ? args.delay : 7500 ;
-			var carouselTransitionSpeed = args && args.speed ? args.speed : 2000; /* transition speed in milliseconds */
+      /* config */
+      var carouselDelay = args && args.delay ? args.delay : 7500;
+      var carouselTransitionSpeed = args && args.speed ? args.speed : 2000; /* transition speed in milliseconds */
 
       /* end config */
 
@@ -19,103 +19,116 @@
 
       /* setup vars */
       var $this = $(this);
-  		var $carousel = $this.find('div.carousel_container');
-  		var $pagination = $this.find('div.carousel_pagination');
-		  var $window = $(window);
-		  /* end setup vars */
+      var $carousel = $this.find('div.carousel_container');
+      var $pagination = $this.find('div.carousel_pagination');
+      var $window = $(window);
+      /* end setup vars */
 
 
 
-		  /* activate carousel */
-			function activateCarouselItem(el) {
-			  $this.find("ul li.carousel_item.active").fadeOut(carouselTransitionSpeed).removeClass('active');
-			  el.fadeIn(carouselTransitionSpeed).addClass('active');
-			};
-			/* end activate carousel */
-
-
-			
-			/* resize window */
-		  $window.resize(function() {
-		    var active_item = $this.find("ul.list-unstyled li:visible");
-		    $carousel.height(active_item.outerHeight());
-		  }).resize();
-		  /* end resize window */
+      /* activate carousel */
+      function activateCarouselItem(el) {
+        $this.find("ul li.carousel_item.active").fadeOut(carouselTransitionSpeed).removeClass('active');
+        el.fadeIn(carouselTransitionSpeed).addClass('active');
+      }
+      /* end activate carousel */
 
 
 
-	 		$(document).ready(function(){
-	    	
-	    	var active_item = $this.find("ul.list-unstyled li:visible");
-
-				/* start carousel */
-				if($this.length) {
-				  
-				  /* activate first item on page load */
-			    activateCarouselItem($this.find("ul li.carousel_item").first());
-					
-	    		$carousel.height(active_item.outerHeight());
-
-			    /* activate first pagination item on page load */
-			    $pagination.find("a").first().addClass('active');
-
-			    /* set carousel in motion */
-			    setInterval(function(){ activateNextCarouselItem() }, carouselDelay);
-
-					/* carousel pagination */
-					$(document).on("click", "div.carousel_pagination a", function(){
-					  if($(this).hasClass('active')) { return false; };
-					  $pagination.find("a").removeClass('active');
-					  activateCarouselItem( $($(this).attr('href')) );
-					  $(this).addClass('active');
-					  return false;
-					});
-
-
-		  };
-		  /* end start carousel */
-
-
-	  	});
+      /* resize window */
+      $window.resize(function () {
+        var active_item = $this.find("ul.list-unstyled li:visible");
+        $carousel.height(active_item.outerHeight());
+      }).resize();
+      /* end resize window */
 
 
 
+      $(document).ready(function () {
 
- 		 	/* start activateNextCarouselItem */
-			function activateNextCarouselItem() {
-			  
-			  if( $this.find("ul li.carousel_item:last").hasClass('active') ) {
-			   	
-			    /* if on last item, go to first */ 
+        var active_item = $this.find("ul.list-unstyled li:visible");
 
-			    activateCarouselItem($this.find("ul li.carousel_item").first());
-			    
-			    /* deactivate current pagination item */
-			    var current_page = $pagination.find("a.active").removeClass('active');
-			    
-			    /* activate next pagination item */
-			    $pagination.find("a").first().addClass('active');
+        /* start carousel */
+        if ($this.length) {
 
-			  } else {
+          /* activate first item on page load */
+          activateCarouselItem($this.find("ul li.carousel_item").first());
 
-			    /* else go to next item */
+          $carousel.height(active_item.outerHeight());
 
-			    activateCarouselItem($this.find("ul li.carousel_item.active").next("li.carousel_item"));
-			    
-			    /* deactivate current pagination item */
-			    var current_page = $pagination.find("a.active").removeClass('active');
-			    
-			    /* activate next pagination item */
-			    $(current_page).next("a").addClass('active');
+          /* activate first pagination item on page load */
+          $pagination.find("a").first().addClass('active');
 
-			  };
-			};
+          /* set carousel in motion */
+          setInterval(function () { activateNextCarouselItem(); }, carouselDelay);
 
- 		  /* end activateNextCarouselItem */
+          /* carousel pagination */
+          $(document).on("click", "div.carousel_pagination a", function () {
+            if ($(this).hasClass('active')) { return false; }
+            $pagination.find("a").removeClass('active');
+
+            /* can use either href pointing at carousel item id or data-carousel-target="x" pointing at data-carousel-item="x" */
+
+            var href = $(this).attr('href');
+            var target = '';
+
+            if (href !== null && href != '#') { 
+              target = href; 
+            } else { 
+              target = $(this).data('carousel-target'); 
+              target = '[data-carousel-item="' + target + '"]';
+            }
+
+            activateCarouselItem($(target));
+            $(this).addClass('active');
+            return false;
+          });
 
 
-		}) /* end this each */
+        }
+      /* end start carousel */
 
-	} /* end some frills */
 
-})( jQuery );
+      });
+
+
+
+
+      /* start activateNextCarouselItem */
+      function activateNextCarouselItem() {
+
+        if ($this.find("ul li.carousel_item:last").hasClass('active')) {
+
+          /* if on last item, go to first */
+
+          activateCarouselItem($this.find("ul li.carousel_item").first());
+
+          /* deactivate current pagination item */
+          var current_page = $pagination.find("a.active").removeClass('active');
+
+          /* activate next pagination item */
+          $pagination.find("a").first().addClass('active');
+
+        } else {
+
+          /* else go to next item */
+
+          activateCarouselItem($this.find("ul li.carousel_item.active").next("li.carousel_item"));
+
+          /* deactivate current pagination item */
+          var current_page = $pagination.find("a.active").removeClass('active');
+
+          /* activate next pagination item */
+          $(current_page).next("a").addClass('active');
+
+        }
+      }
+
+      /* end activateNextCarouselItem */
+
+
+    }); /* end this each */
+
+  }; /* end some frills */
+
+})(jQuery);
